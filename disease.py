ba21f693503bb4ce2e2496b90dd6856c1673ccf3
PADMAPRIYAPADHU <135614314+PADMAPRIYAPADHU@users.noreply.github.com>
2024-04-23 09:45:14 +0530

    Add files via upload

---
+++ disease.py
@@ -0,0 +1,41 @@
+crop_diseases = {
+    'Citrus': {
+        'Aphids': ['Citrus tristeza virus', 'Citrus Canker'],
+        'Brown Plant Hopper': ['Citrus variegated chlorosis'],
+        'Citrus Leaf Miner': ['Citrus canker'],
+        'Whiteflies': ['Citrus tristeza virus']
+    },
+    'Paddy': {
+        'Fall Army Worms': ['Rice tungro virus', 'Rice blast'],
+        'Grasshopper': ['Rice blast', 'Bacterial leaf streak'],
+        'Locust': ['Rice blast'],
+        'Stem Borer': ['Rice blast'],
+        'Brown Plant Hopper':['Rice tungro disease']
+    },
+    'Cotton': {
+        'Aphids': ['Cotton leaf curl virus', 'Cotton bollworm'],
+        'Whiteflies': ['Cotton leaf curl virus']
+    },
+    'Mango': {
+        'Aphids': ['Powdery mildew'],
+        'Thrips': ['Anthracnose']
+    },
+    'Tomato': {
+        'Aphids': ['Tomato yellow leaf curl virus'],
+        'Whiteflies': ['Tomato yellow leaf curl virus'],
+        'Stem Borer': ['Tomato fruit borer']
+    },
+    'Ground nuts': {
+        'Fall Army Worms': ['Peanut bud necrosis virus'],
+        'Whiteflies': ['Groundnut rosette'],
+        'Stem Borer': ['Peanut stem necrosis virus']
+    },
+    'Lady\'s finger': {
+        'Aphids': ['Okra leaf curl virus'],
+        'Whiteflies': ['Okra yellow vein mosaic virus']
+    },
+    'Pomegranate': {
+        'Aphids': ['Pomegranate aphid'],
+        'Whiteflies': ['Pomegranate whitefly']
+    }
+}
